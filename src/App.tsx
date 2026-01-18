eab29c4180cfa2d4a3fc3aefa9f22d08367c1d2f
Kyle Wilson <kyle.j.wilson@outlook.com>
2026-01-18 15:17:29 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Container, Typography } from '@mui/material'
+
+function App() {
+
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h1">Hello World</Typography>
+    </Container>
+  )
+}
+
+export default App
